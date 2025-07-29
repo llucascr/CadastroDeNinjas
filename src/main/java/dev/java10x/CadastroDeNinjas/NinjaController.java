408bb277998d6e9080edda9e106f4c3c9a4c8276
llucascr <lucasdecamposranzani@gmail.com>
2025-07-29 17:32:49 -0300

    feat: Issue Configurar classe Ninja #2

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1")
-public class Controller {
+public class NinjaController {
 
     @GetMapping(path = "/boasVindas")
     public String boasVindas() {
