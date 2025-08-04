dafd04577a7a3bc0f931c249f1b85f31ddc96b8b
llucascr <lucasdecamposranzani@gmail.com>
2025-08-04 17:43:30 -0300

    fix: adiciona a extenção do JpaRepository a camada de NinjaRepository

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Repository/NinjaRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Repository/NinjaRepository.java
@@ -1,4 +1,7 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.Repository;
 
-public interface NinjaRepository {
+import dev.java10x.CadastroDeNinjas.Ninjas.Model.Ninja;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<Ninja, Long> {
 }
